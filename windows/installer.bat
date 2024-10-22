79701b35ac9d2ee7f5f8288b975789756ed25d6d
Tudgwal <jetfly42@gmail.com>
2024-10-22 18:08:09 +0200

    fix installer.bat

--- windows/installer.bat
+++ windows/installer.bat
@@ -2,7 +2,6 @@
 title Installing Chocolatey...
 
 echo This script will install Chocolatey on your Windows system.
-pause
 
 REM Check if running as administrator
 net session >nul 2>&1
@@ -18,25 +17,29 @@ powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.
 REM Check if Chocolatey was installed successfully
 if exist "%ALLUSERSPROFILE%\chocolatey\bin\choco.exe" (
     echo Chocolatey has been installed successfully!
-    pause
 ) else (
     echo Failed to install Chocolatey. Please try again.
     pause
     exit /b 1
 )
 
-REM Install imageMagick using Chocolatey
-echo Installing ImageMagick using Chocolatey...
-choco install ImageMagick -y --force > nul 2>&1
-
-REM Check if ImageMagick was installed successfully
-if exist "C:\Program Files\ImageMagick-7.1.0-Q16" (
-    echo ImageMagick has been installed successfully!
-    pause
-) else (
-    echo Failed to install ImageMagick. Please try again.
-    pause
-    exit /b 1
+REM Check if ImageMagick is already installed
+for /d %%i in ("C:\Program Files\ImageMagick-*") do (
+    if exist "%%i" (
+        echo ImageMagick has been installed successfully!
+    ) else (
+        REM Install ImageMagick using Chocolatey
+        echo Installing ImageMagick using Chocolatey...
+        choco install ImageMagick -y --force
+        REM Check if ImageMagick was installed successfully
+        if exist "C:\Program Files\ImageMagick-7.1.0-Q16" (
+            echo ImageMagick has been installed successfully!
+        ) else (
+            echo Failed to install ImageMagick. Please try again.
+            pause
+            exit /b 1
+        )
+    )
 )
 
 echo Installation completed successfully!
